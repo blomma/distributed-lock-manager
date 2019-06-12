e54a337b66ac9bc37d213213ae89e696800b606f
Mikael Hultgren <blomma@artsoftheinsane.com>
2019-06-12 17:01:29 +0200

    Using

--- Program.cs
+++ Program.cs
@@ -1,20 +1,14 @@
 ﻿using System;
 
-namespace RLock
-{
+namespace RedisDistributedLockManager {
     class Program {
         static void Main(string[] args) {
-            var s = new RedisDistributedLockManager("localhost");
-            var result = s.Lock("test", new TimeSpan(0, 40, 0));
-            if (!result.success) {
-                return;
+            using (var redisDistributedLockManager = new RedisDistributedLockManager("localhost")) {
+                var success = redisDistributedLockManager.Lock("test", new TimeSpan(0, 40, 0));
+                if (!success) {
+                    return;
+                }
             }
-
-            // Do BIZ
-
-
-            // Unlock
-            s.Unlock(result.redisDistributedLock);
         }
     }
 }
--- RedisDistributedLockManager.cs
+++ RedisDistributedLockManager.cs
@@ -1,86 +1,101 @@
-using System;
-using StackExchange.Redis;
+namespace RedisDistributedLockManager {
+    using System;
+    using StackExchange.Redis;
 
-public class RedisDistributedLock
-{
-    public RedisKey Key { get; private set; }
+    public class RedisDistributedLock {
+        public RedisKey Key { get; private set; }
 
-    public RedisValue Value { get; private set; }
+        public RedisValue Value { get; private set; }
 
-    public TimeSpan Validity { get; private set; }
+        public TimeSpan Validity { get; private set; }
 
-    public RedisDistributedLock(RedisKey key, RedisValue value, TimeSpan validity)
-    {
-        Key = key;
-        Value = value;
-        Validity = validity;
+        public RedisDistributedLock(RedisKey key, RedisValue value, TimeSpan validity) {
+            Key = key;
+            Value = value;
+            Validity = validity;
+        }
     }
-}
 
-public class RedisDistributedLockManager
-{
-    private readonly ConnectionMultiplexer redis;
+    public class RedisDistributedLockManager : IDisposable {
+        private readonly ConnectionMultiplexer connectionMultiplexer;
+        private RedisDistributedLock redisDistributedLock;
 
-    public RedisDistributedLockManager(string connectionString)
-    {
-        try
-        {
+        public RedisDistributedLockManager(string connectionString) {
             var options = ConfigurationOptions.Parse(connectionString);
-            redis = ConnectionMultiplexer.Connect(options.ToString());
-        }
-        catch (Exception ex)
-        {
-            // logger.Error(ex, "Failed to connect to Redis");
-        }
-    }
-
-    private bool IsConnected()
-    {
-        if (redis == null)
-        {
-            return false;
+            connectionMultiplexer = ConnectionMultiplexer.Connect(options.ToString());
         }
 
-        return redis.IsConnected;
-    }
-
-    const String UnlockScript = @"
-            if redis.call(""get"",KEYS[1]) == ARGV[1] then
-                return redis.call(""del"",KEYS[1])
-            else
-                return 0
-            end";
-
-    private byte[] CreateUniqueLockId()
-    {
-        return Guid.NewGuid().ToByteArray();
-    }
-
-    public (bool success, RedisDistributedLock redisDistributedLock) Lock(string key, TimeSpan ttl)
-    {
-        if (!IsConnected())
-        {
-            return (success: false, redisDistributedLock: null);
+        public RedisDistributedLockManager(ConnectionMultiplexer connectionMultiplexer) {
+            this.connectionMultiplexer = connectionMultiplexer;
         }
 
-        var value = CreateUniqueLockId();
-        var result = redis.GetDatabase().StringSet(key, value, ttl, When.NotExists);
-        if (!result)
-        {
-            return (success: false, redisDistributedLock: null);
+        private bool IsConnected() {
+            if (connectionMultiplexer == null) {
+                return false;
+            }
+
+            return connectionMultiplexer.IsConnected;
         }
 
-        return (success: true, redisDistributedLock: new RedisDistributedLock(key, value, ttl));
-    }
+        private const String UnlockScript = @"
+                if redis.call(""get"",KEYS[1]) == ARGV[1] then
+                    return redis.call(""del"",KEYS[1])
+                else
+                    return 0
+                end";
 
-    public void Unlock(RedisDistributedLock redisDistributedLock)
-    {
-        RedisKey[] key = { redisDistributedLock.Key };
-        RedisValue[] values = { redisDistributedLock.Value };
-        var result = redis.GetDatabase().ScriptEvaluate(
-            UnlockScript,
-            key,
-            values
-        );
+        private byte[] CreateUniqueLockId() {
+            return Guid.NewGuid().ToByteArray();
+        }
+
+        public bool Lock(string key, TimeSpan ttl) {
+            if (!IsConnected()) {
+                return false;
+            }
+
+            var value = CreateUniqueLockId();
+            var result = connectionMultiplexer.GetDatabase().StringSet(key, value, ttl, When.NotExists);
+            if (!result) {
+                return false;
+            }
+
+            redisDistributedLock = new RedisDistributedLock(key, value, ttl);
+
+            return true;
+        }
+
+        public void Unlock() {
+            if (redisDistributedLock == null) {
+                return;
+            }
+
+            RedisKey[] key = { redisDistributedLock.Key };
+            RedisValue[] values = { redisDistributedLock.Value };
+            var result = connectionMultiplexer.GetDatabase().ScriptEvaluate(
+                UnlockScript,
+                key,
+                values
+            );
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing) {
+            if (!disposedValue) {
+                if (disposing) {
+                    Unlock();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose() {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+        #endregion
     }
 }
